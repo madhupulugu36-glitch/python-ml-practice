c5bd419889b5ae8fe44f5e9accf4a21897950df3
Madhu <madhupulugu36@gmail.com>
2026-08-20 17:32:02 +0530

    Practice Python data types

---
+++ 01_Python_Basics/data_types.py
@@ -0,0 +1,47 @@
+integer_value = 25
+float_value = 25.5
+string_value = "Python"
+boolean_value = True
+
+print("Integer Value: ", integer_value)
+print("Float Value: ", float_value)
+print("String Value: ", string_value)
+print("Boolean Value: ", boolean_value)
+
+print(type(integer_value))
+print(type(float_value))
+print(type(string_value))
+print(type(boolean_value))
+
+
+#Complex Number
+complex_value = 3 + 4j
+print("Complex Value:", complex_value)
+print("Type of Complex Value:", type(complex_value))
+
+#List
+fruits = ["Apple", "Banana", "Cherry", "Mango"]
+print("List:", fruits)
+print(type(fruits))
+
+#Tuple
+coordinates = (10, 20)
+print("Tuple:", coordinates)
+print(type(coordinates))
+
+
+#Set
+
+numbers = {100, 200, 300, 400}
+print("Set:", numbers)
+print(type(numbers))
+
+#Dictionary
+student = {"name": "John", "age": 19, "status": "Passed"}
+print("Dictionary:", student)
+print(type(student))
+
+#None
+result = None
+print("None value:", result)
+print(type(result))
