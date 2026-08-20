72d4f3ab51480d35f81748e40444887b78f83353
Madhu <madhupulugu36@gmail.com>
2026-08-20 15:26:21 +0530

    Practice Python variables examples

--- 01_Python_Basics/variables.py
+++ 01_Python_Basics/variables.py
@@ -5,3 +5,65 @@ city = "Hyderabad"
 print(name)
 print(course)
 print(city)
+
+print("-------------------")
+
+#Student Information
+student_name = "Rahul"
+student_age = 20
+student_grade = "A"
+
+print(student_name)
+print(student_age)
+print(student_grade)
+
+
+print("-------------------")
+
+
+#Product Information
+product_name = "Laptop"
+product_price = 55000
+product_rating = 4.5
+product_available = True
+
+print(product_name)
+print(product_price)    
+print(product_rating)
+print(product_available)
+
+print("-------------------")
+
+
+#Basic Arithmetic Operations
+number1 = 30
+number2 = 8
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+
+print("-------------------")
+
+#Variable Reassignment
+score = 152
+print("Old score: ", score)
+
+score = 175
+print("New score: ", score)
+
+print("-------------------")
+
+#Swapping Variable Values
+a = 100
+b = 300
+a, b = b, a
+
+print("a: ", a)
+print("b: ", b)
