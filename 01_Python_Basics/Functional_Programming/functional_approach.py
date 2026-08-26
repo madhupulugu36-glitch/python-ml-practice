44d80ea289f96664f75d1b3269630aa0158f60d7
Madhu <madhupulugu36@gmail.com>
2026-08-26 17:08:22 +0530

    Practice Functional Programming

---
+++ 01_Python_Basics/Functional_Programming/functional_approach.py
@@ -0,0 +1,5 @@
+numbers = [11, 12, 13, 14, 15]
+
+result = list(map(lambda x: x * 2, numbers))
+
+print(result)
---
+++ 01_Python_Basics/Functional_Programming/traditional_approach.py
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 4, 5]
+
+result = []
+
+for number in numbers:
+    result.append(number * 2)
+print(result)
