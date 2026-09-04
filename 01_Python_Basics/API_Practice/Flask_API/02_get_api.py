86668b088245298e1335cdb0711b80390b309d71
Madhu <madhupulugu36@gmail.com>
2026-09-04 14:57:12 +0530

    Updated Flask GET API

--- 01_Python_Basics/API_Practice/Flask_API/02_get_api.py
+++ 01_Python_Basics/API_Practice/Flask_API/02_get_api.py
@@ -2,15 +2,32 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/student")
-def student():
-    data = {
-        "id": 1,
-        "name": "Madhu",
-        "course": "Python ML"
-    }
 
-    return jsonify(data)
+@app.route("/students/<int:id>")
+def student_by_id(id):
+
+    students = [
+        {
+            "id": 1,
+            "name": "Madhu",
+            "course": "Python ML"
+        },
+        {
+            "id": 2,
+            "name": "Rahul",
+            "course": "Data Science"
+        },
+        {
+            "id": 3,
+            "name": "Priya",
+            "course": "Python"
+        }
+    ]
+
+    for student in students:
+            if student["id"] == id:
+                 return jsonify(student)
+    return jsonify({"message": "Student not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
