463e4dcba88c6211c3cb53e5e6d01503b41a5d85
Madhu <madhupulugu36@gmail.com>
2026-09-17 12:14:41 +0530

    Use environment variables for MySQL connection

--- 01_Python_Basics/Flask_SQLAlchemy_API/db.py
+++ 01_Python_Basics/Flask_SQLAlchemy_API/db.py
@@ -1,15 +1,23 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 from sqlalchemy.engine import URL
+
 from models import Base
+
+
+# Load variables from .env
+load_dotenv()
 
 
 DATABASE_URL = URL.create(
     drivername="mysql+pymysql",
-    username="root",
-    password="Madhu8466",
-    host="localhost",
-    port=3306,
-    database="sqlalchemy_01"
+    username=os.getenv("MYSQL_USERNAME"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=int(os.getenv("MYSQL_PORT")),
+    database=os.getenv("MYSQL_DATABASE")
 )
 
 
@@ -23,11 +31,10 @@ try:
         print("MySQL connection successful")
         print("Result:", result.scalar())
 
+    Base.metadata.create_all(engine)
+
+    print("Employees table checked successfully")
+
 except Exception as e:
     print("Database connection failed")
     print("Error:", e)
-
-
-Base.metadata.create_all(engine)
-
-print("Employees table checked successfully")
